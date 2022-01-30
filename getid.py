731896b01e8fa776b32165477b32a7d86bffa88c
hwy0010 <hwy0010@users.noreply.github.com>
2022-01-30 22:13:01 +0800

    Update getid.py

--- getid.py
+++ getid.py
@@ -49,9 +49,7 @@ def getid():
 if __name__ == '__main__':
     id = getid()
     if id!='00000':
-        f = open('id','w')
-        f.write(id+'\n')
-        f.close()
+        print ("ID: "+id)
     else:
         print ("学号错误")
         exit(0)
